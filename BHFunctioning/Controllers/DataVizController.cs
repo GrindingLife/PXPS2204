65912df30a84b5ed42a4e72386d69881cf6cb466
Bill Tran <bill.tran54499@gmail.com>
2022-03-18 18:48:58 +1100

    Add project files.

---
+++ BHFunctioning/Controllers/DataVizController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BHFunctioning.Models;
+using BHFunctioning.Data;
+namespace BHFunctioning.Controllers
+{
+    public class DataVizController : Controller
+    {
+        // GET: DataVizController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: DataVizController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DataVizController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DataVizController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DataVizController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DataVizController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DataVizController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DataVizController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ BHFunctioning/Models/HealthData.cs
@@ -0,0 +1,30 @@
+﻿using BHFunctioning.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace BHFunctioning.Models
+{
+    public class HealthData
+    {
+        private ApplicationDbContext _db;
+        [Key]
+        public int Id { get; set; }
+        [Range(0,1)]
+        public int NEET { get; set; }
+        [Range(0, 1)]
+        public int Selfharm { get; set; }
+        [Range(0, 1)]
+        public int Psychosis { get; set; }
+        [Range(0, 1)]
+        public int Medical { get; set; }
+        [Range(0, 1)]
+        public int ChildDx { get; set; }
+        [Range(0, 1)]
+        public int Circadian { get; set; }
+        [Range(1, 4)]
+        public int Tripartite { get; set; }
+        [Range(1, 3)]
+        public int ClinicalStage { get; set; }
+        [Range(1, 5)]
+        public int Sofas { get; set; }
+    }
+}
